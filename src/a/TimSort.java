b36336d8822a3f45644a8f05125c3fcd69d2af00
majutestoni <majuliatestoni@gmail.com>
2023-06-16 15:23:39 -0300

    ajuste

--- src/a/TimSort.java
+++ src/a/TimSort.java
@@ -6,83 +6,88 @@ public class TimSort<T extends Comparable<T>> implements Sort<T> {
 	
 	// Inicia com o caulculo da minRun
 	// quem é o tamanhos dos subVetores
-	
 	@Override
 	public void sort(T[] vetor) {
-		this.array = vetor;
-
+        int n = vetor.length;
 		int minRun = calculateMinRun(vetor.length);
+        
+        for (int start = 0; start < n; start += minRun) {
+            int end = Math.min(start + minRun - 1, n - 1);
+            insertionSort(vetor, start, end);
+        }
+        
+        int size = minRun;
+        while (size < n) {
+            for (int left = 0; left < n; left += 2 * size) {
+                int mid = Math.min(n - 1, left + size - 1);
+                int right = Math.min(left + 2 * size - 1, n - 1);
+                
+                if (mid < right) {
+                    merge(vetor, left, mid, right);
+                }
+            }
+            
+            size = 2 * size;
+        }
+    }
+	
+    public void insertionSort(T[] vetor, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int j = i;
+            while (j > left && vetor[j].compareTo(vetor[j - 1]) < 0) {
+                T temp = vetor[j];
+                vetor[j] = vetor[j - 1];
+                vetor[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+    
+    public void merge(T[] vetor, int l, int m, int r) {
+        int len1 = m - l + 1;
+        int len2 = r - m;
+        T[] left = (T[]) new Object[len1];
+        T[] right = (T[]) new Object[len2];
+        
+        for (int i = 0; i < len1; i++) {
+            left[i] = vetor[l + i];
+        }
+        
+        for (int i = 0; i < len2; i++) {
+            right[i] = vetor[m + 1 + i];
+        }
+        
+        int i = 0, j = 0, k = l;
+        
+        while (i < len1 && j < len2) {
+            if (left[i].compareTo(right[j]) <= 0) {
+                vetor[k] = left[i];
+                i++;
+            } else {
+                vetor[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < len1) {
+            vetor[k] = left[i];
+            k++;
+            i++;
+        }
+        
+        while (j < len2) {
+            vetor[k] = right[j];
+            k++;
+            j++;
+        }
+    }
 
-		for (int i = 0; i < vetor.length; i += minRun) {
-			// int end = i + minRun;
-			int end = Math.min(i + minRun - 1, vetor.length - 1);
-			insertionSort(i, end);
-		}
-		
-		// primeiro ele insere os subvetores de maneira eficiente
-
-		for (int j = minRun; j < vetor.length; j = 2 * j) {
-			for (int left = 0; left < vetor.length; left += 2 * j) {
-				int mid = left + j - 1;
-				int right = Math.min(left + 2 * j - 1, vetor.length - 1);
-				merge(left, mid, right);
-			}
-		}
-
-	}
-
-	private void merge(int left, int mid, int right) {
-		int length1 = mid - left + 1;
-		int length2 = right - mid;
-
-		T[] leftArray = (T[]) new Object[length1];
-		T[] rightArray = (T[]) new Object[length2];
-
-		int i = 0, j = 0, k = left;
-
-		while (i < length1 && j < length2) {
-			if (leftArray[i].compareTo(rightArray[j]) <= 0) {
-				array[k] = leftArray[i];
-				i++;
-			} else {
-				array[k] = rightArray[j];
-				j++;
-			}
-			k++;
-		}
-
-		while (i < length1) {
-			array[k] = leftArray[i];
-			i++;
-			k++;
-		}
-
-		while (j < length2) {
-			array[k] = rightArray[j];
-			j++;
-			k++;
-		}
-	}
-
-	private void insertionSort(int start, int end) {
-		
-		// aqui insere
-		
-		for (int i = start + 1; i <= end; i++) {
-			T key = array[i];
-			int j = i - 1;
-
-			while (j >= start && array[j].compareTo(key) > 0) {
-				array[j + 1] = array[j];
-				j--;
-			}
-
-			array[j + 1] = key;
-		}
-	}
-
+	
+	
 	private int calculateMinRun(int length) {
 		int r = 0;
+		// 32 é a diferença entre 64 e 32
 		while (length >= 32) {
 			// r faz um OR bit a bit com o resultado da expressão "(n & 1)"
 			r |= (length & 1);
